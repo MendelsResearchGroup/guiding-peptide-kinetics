21868b868adf5876a464d1e82ea797f97271e2bb
Alexander Zhilkin <alexander.z@zeus.zeus.technion.ac.il>
2025-08-27 15:18:39 +0300

    Add new mutations

--- src/common/consts.py
+++ src/common/consts.py
@@ -18,20 +18,21 @@ mutation_map = {
     "YYDPETGQWY": "T7Q",
     "YYDPETGRWY": "T7R",
     "YYDPETGYWY": "T7Y",
+    "YYDPETGGWY": "T7G",
     "chignolin": "Wildtype",
 }
 
 
 groupByResidue = {
     2: ["D2A", "D2C", "D2M", "D2N", "D2R", "D2E"],
-    7: ["T7V", "T7Q", "T7R", "T7Y"],
+    7: ["T7V", "T7Q", "T7R", "T7Y", 'T7G'],
     9: ["Y9E", "Y9G", "Y9Q", "Y9R", "Y9V", "Y9A"],
 }
 
 groupByProperty = {
     "RHKDE": ["D2R", "D2E", "Y9E", "Y9R", "T7R"],
     "STNQ": ["D2N", "Y9Q", "T7Q"],
-    "CUGP": ["D2C", "Y9G"],
+    "CUGP": ["D2C", "Y9G", "T7G"],
     "AVILMFYW": ["D2A", "D2M", "Y9A", "Y9V", "T7V", "T7Y"],
 }
 
@@ -51,6 +52,8 @@ proteins = [
     "YYDPETGYWY",
     "YYDPETGTWV",
     "YYDPETGRWY",
+    # "YYDPETGTWA",
+    # "YYDPETGGWY"
 ]
 
-thresholds = np.arange(0.1, 0.50, 0.04)
+thresholds = np.arange(0.2, 0.50 + 1e-9, 0.04)
