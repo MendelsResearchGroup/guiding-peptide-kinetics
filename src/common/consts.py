6035d764efb47e89c5468797e751187c7f08c9fb
Alexander Zhilkin <alexander.z@zeus-post.zeus.technion.ac.il>
2025-09-07 11:55:43 +0300

    Add new mutations

--- src/common/consts.py
+++ src/common/consts.py
@@ -21,12 +21,22 @@ mutation_map = {
     "YYDPETGGWY": "T7G",
     "YYDPETGDWY": "T7D",
     "chignolin": "WT",
+
+    "AYDPETGTWY": "Y0A",
+    "YYDCETGTWY": "P3C",
+    "RYDPETGTWY": "Y0R",
+    "QYDPETGTWY": "Y0Q",
+    "EYDPETGTWY": "Y0E",
+    "YYDMETGTWY"    : "P3M",
+    # "YYDDETGTWY"    : "P3D",
     
 }
 
 
 groupByResidue = {
+    0: ["Y0A", "Y0R", "Y0Q", "Y0E"],
     2: ["D2A", "D2C", "D2M", "D2N", "D2R", "D2E"],
+    3: ["P3C", "P3M"],
     7: ["T7V", "T7Q", "T7R", "T7Y", 'T7G', 'T7D'],
     9: ["Y9E", "Y9G", "Y9Q", "Y9R", "Y9V", "Y9A"],
 }
@@ -35,34 +45,16 @@ groupByProperty = {
     "RHKDE": ["D2R", "D2E", "Y9E", "Y9R", "T7R"],
     "STNQ": ["D2N", "Y9Q", "T7Q"],
     "CUGP": ["D2C", "Y9G", "T7G", "T7D"],
-    "AVILMFYW": ["D2A", "D2M", "Y9A", "Y9V", "T7V", "T7Y"],
+    "AVILMFYW": ["D2A", "D2M", "Y9A", "Y9V", "T7V", "T7Y", "Y0A"],
 }
 
-proteins = [
-    "chignolin",
-    "YYCPETGTWY",
-    "YYDPETGTWE",
-    "YYRPETGTWY",
-    "YYAPETGTWY",
-    "YYDPETGQWY",
-    "YYDPETGTWG",
-    "YYDPETGTWQ",
-    "YYNPETGTWY",
-    "YYEPETGTWY",
-    "YYDPETGTWR",
-    "YYDPETGVWY",
-    "YYDPETGYWY",
-    "YYDPETGTWV",
-    "YYDPETGRWY",
-    "YYDPETGTWA",
-    "YYDPETGGWY",
-    "YYMPETGTWY", 
-    "YYDPETGDWY"
-]
+proteins = list(mutation_map.keys())
 
 thresholds = np.arange(0.1, 0.50 + 1e-9, 0.04)
 res_colors = {
+    0: "red",
     2: "orange",
+    3: "purple",
     7: "blue",
     9: "green"
 }
--- src/fpt_single_run_job.sh
+++ src/fpt_single_run_job.sh
@@ -22,7 +22,7 @@ GRO="npt.gro"
 CPT="npt.cpt"
 TOP="topol.top"
 REF="reference.pdb"
-PLUMED_TEMPLATE="src/fpt_plumed/fpt_plumed_$PROTEIN.dat"
+PLUMED_TEMPLATE="src/fpt_plumed/$PROTEIN.dat"
 NSTEPS=40000000
 
 RUN_ID=$(printf "%03d" "$ID")
