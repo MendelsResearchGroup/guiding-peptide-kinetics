e7f7ec1d0063e87fb0d90e3210eecb3633853f63
Alexander Zhilkin <alexander.z@zeus.zeus.technion.ac.il>
2025-12-04 14:12:35 +0200

    New thresholds calculated

--- src/common/consts.py
+++ src/common/consts.py
@@ -29,8 +29,8 @@ long_to_short = {
     "YYDDETGTWY": "P3D",
     # "YYDRETGTWY": "P3R",
     "YYDCETGTWY": "P3C",
-    # "YYDPERGTWY": "T5R",
-    # "YYDPEGGTWY": "T5G",
+    "YYDPERGTWY": "T5R",
+    "YYDPEGGTWY": "T5G",
     # "YYDPEYGTWY": "T5Y", Not yet sampled
     "YYDPATGTWY": "E4A",
     "YYDPGTGTWY": "E4G",
