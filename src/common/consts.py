f586e645c11848780a3746fdb0c125039a20ffee
Alex Zhilkin <alexander.z@campus.technion.ac.il>
2025-12-04 08:26:15 +0200

    Removed outlier, now theres good correlation

--- src/common/consts.py
+++ src/common/consts.py
@@ -27,15 +27,15 @@ long_to_short = {
     "AYDPETGTWY": "Y0A",
     "YYDMETGTWY": "P3M",
     "YYDDETGTWY": "P3D",
-    "YYDRETGTWY": "P3R",
+    # "YYDRETGTWY": "P3R",
     "YYDCETGTWY": "P3C",
-    "YYDPERGTWY": "T5R",
-    "YYDPEGGTWY": "T5G",
+    # "YYDPERGTWY": "T5R",
+    # "YYDPEGGTWY": "T5G",
     # "YYDPEYGTWY": "T5Y", Not yet sampled
     "YYDPATGTWY": "E4A",
     "YYDPGTGTWY": "E4G",
     "YYDPRTGTWY": "E4R",
-    "YYDPYTGTWY": "E4Y",
+    # "YYDPYTGTWY": "E4Y", # Starts from high rmsd
     "YYKPETGTWY": "D2K",
     "YYYPETGTWY": "D2Y",
     # "YYDPETGTWK": "Y9K", outlier?
